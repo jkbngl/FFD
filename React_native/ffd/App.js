ac7bb0d5f6bc1b22b9e3c3f59a2bf4c4a7f0e58a
jkbngl <jakob.engl@hotmail.de>
2019-10-22 22:33:09 +0200

    #4 working version with bottom navigation bar

--- React_native/ffd/App.js
+++ React_native/ffd/App.js
@@ -1,16 +1,58 @@
-import React, { Component } from 'react';
-import { Text, StyleSheet, View } from 'react-native';
-import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createAppContainer } from 'react-navigation';
 
-export default class HelloWorldApp extends Component {
+import Icon from "react-native-vector-icons/FontAwesome";
+
+export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
+        <AppContainer />
+    );
+  }
+}
+
+class HomeScreen extends React.Component {
+  render() {
+    return(
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <View style={styles.container}>
         <View style={styles.alternativeLayoutButtonContainer}>
           <Text>Budget:{"\n"}900</Text>
-
           <Text style={{color: 'green'}}>Actual:{"\n"}400</Text>
         </View>
+      </View>
+      </View>
+    );
+  }
+}
+
+class ExploreScreen extends React.Component {
+  render() {
+    return(
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#d0d0d0'}}>
+        <Text> This is my Explore screen </Text>
+      </View>
+    );
+  }
+}
+
+class NotificationsScreen extends React.Component {
+  render() {
+    return(
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff'}}>
+        <Text> This is my Notifications screen </Text>
+      </View>
+    );
+  }
+}
+
+class ProfileScreen extends React.Component {
+  render() {
+    return(
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#d0d0d0'}}>
+        <Text> This is my Profile screen </Text>
       </View>
     );
   }
@@ -22,8 +64,53 @@ const styles = StyleSheet.create({
    justifyContent: 'center',
   },
   alternativeLayoutButtonContainer: {
-    margin: 100,
+    margin: 5,
     flexDirection: 'row',
     justifyContent: 'space-between'
   }
 });
+
+const bottomTabNavigator = createBottomTabNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="home" size={25} color={tintColor} />
+        )
+      }
+    },
+    Explore: {
+      screen: ExploreScreen,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="comments" size={25} color={tintColor} />
+        )
+      }
+    },
+    Notifications: {
+      screen: NotificationsScreen,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="search" size={25} color={tintColor} />
+        )
+      }
+    },
+    Profile: {
+      screen: ProfileScreen,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="user" size={25} color={tintColor} />
+        )
+      }
+    },
+  },
+  {
+    initialRouteName: 'Home',
+    tabBarOptions: {
+      activeTintColor: '#eb6e3d'
+    }
+  }
+);
+
+const AppContainer = createAppContainer(bottomTabNavigator);
