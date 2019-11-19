8d2f20d44c031c437c255c12a1ce241382300680
jkbngl <jakob.engl@hotmail.de>
2019-11-19 21:50:34 +0100

    #8 working connecting to DB

--- Backend/people.py
+++ Backend/people.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+import psycopg2
+
 
 def get_timestamp():
     return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
@@ -22,6 +24,34 @@ PEOPLE = {
     }
 }
 
+def connect():
+    try:
+        connection = psycopg2.connect(user = <user>,
+                                      password = <password>,
+                                      host = "192.168.0.20",
+                                      port = "5432",
+                                      database = "postgres")
+        
+        # Print PostgreSQL Connection properties
+        cursor = connection.cursor()
+        print(connection.get_dsn_parameters(),"\n")
+
+        # Print PostgreSQL version
+        cursor.execute("SELECT version();")
+        record = cursor.fetchone()
+        print("You are connected to - ", record,"\n")
+
+        return connection
+
+    except (Exception, psycopg2.Error) as error :
+        print ("Error while connecting to PostgreSQL", error)
+    #finally:
+    #    #closing database connection.
+    #        if(connection):
+    #            cursor.close()
+    #            connection.close()
+    #            print("PostgreSQL connection is closed")
+
 # Create a handler for our read (GET) people
 def read():
     """
@@ -30,5 +60,14 @@ def read():
 
     :return:        sorted list of people
     """
+    connection = connect()
+    cursor = connection.cursor()
+
+    postgreSQL_select_Query = "select * from tp_plan"
+    cursor.execute(postgreSQL_select_Query)
+    mobile_records = cursor.fetchall() 
+
+    return mobile_records
+
     # Create the list of people from our data
-    return [PEOPLE[key] for key in sorted(PEOPLE.keys())]
+    #return [PEOPLE[key] for key in sorted(PEOPLE.keys())]
