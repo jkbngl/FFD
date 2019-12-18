2b6e2e5743e0a100298f93c53c8a600b7bb42570
jkbngl <jakob.engl@hotmail.de>
2019-12-18 22:16:28 +0100

    #21 #11 debugging for finding how to handle routes

--- flutter/ffd/lib/main.dart
+++ flutter/ffd/lib/main.dart
@@ -163,10 +163,10 @@ class _DestinationViewState extends State<DestinationView> {
           settings: settings,
           builder: (BuildContext context) {
             switch(settings.name) {
-              case '/':
-                print('The name of the settings is: $settings.name');
-                print('The destination of the widget is: $widget.destination');
+              print('The name of the settings is: $settings.name');
+              print('The destination of the widget is: $widget.destination');
 
+              case '/':
                 return RootPage(destination: widget.destination);
               case '/list':
                 return ListPage(destination: widget.destination);
