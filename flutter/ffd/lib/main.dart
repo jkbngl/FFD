1efb1ad5a710d6d89f9d20446ec2b63a620526b3
jkbngl <jakob.engl@hotmail.de>
2019-12-20 22:23:40 +0100

    #21 #11 test with switching between pages with bottom nav bar

--- flutter/ffd/lib/main.dart
+++ flutter/ffd/lib/main.dart
@@ -1,59 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 
-void main() => runApp(MaterialApp(home: BottomNavBar()));
-
-class BottomNavBar extends StatefulWidget {
+class AppFooter extends StatefulWidget {
   @override
-  _BottomNavBarState createState() => _BottomNavBarState();
+  _AppFooterState createState() => _AppFooterState();
 }
 
-class _BottomNavBarState extends State<BottomNavBar> {
-  int _page = 0;
-  GlobalKey _bottomNavigationKey = GlobalKey();
+class _AppFooterState extends State<AppFooter> {
+  int _currentIndex = 0;
+
+  List<Widget> _pages = [
+    Text("page1"),
+    Text("page2"),
+    Text("page3"),
+  ];
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        bottomNavigationBar: CurvedNavigationBar(
-          key: _bottomNavigationKey,
-          index: 0,
-          height: 50.0,
-          items: <Widget>[
-            Icon(Icons.home, size: 30),
-            Icon(Icons.attach_money, size: 30),
-            Icon(Icons.compare_arrows, size: 30),
-            Icon(Icons.account_balance_wallet, size: 30),
-            Icon(Icons.settings, size: 30),
-          ],
-          color: Colors.white,
-          buttonBackgroundColor: Colors.white,
-          backgroundColor: Colors.blueAccent,
-          animationCurve: Curves.easeInOut,
-          animationDuration: Duration(milliseconds: 250),
-          onTap: (index) {
-            setState(() {
-              _page = index;
-            });
-          },
-        ),
-        body: Container(
-          color: Colors.blueAccent,
-          child: Center(
-            child: Column(
-              children: <Widget>[
-                Text(_page.toString(), textScaleFactor: 10.0),
-                RaisedButton(
-                  child: Text('Go To Page of index 1'),
-                  onPressed: () {
-                    final CurvedNavigationBarState navBarState =
-                        _bottomNavigationKey.currentState;
-                    navBarState.setPage(1);
-                  },
-                )
-              ],
-            ),
-          ),
-        ));
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _currentIndex,
+        onTap: (int index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: [
+          new BottomNavigationBarItem(
+              backgroundColor: Colors.white,
+              icon: _currentIndex == 0
+                  ? new Image.asset('assets/images/dashboard_active.png')
+                  : new Image.asset('assets/images/dashboard_inactive.png'),
+              title:
+              new Text('Dashboard', style: new TextStyle(fontSize: 12.0))),
+          new BottomNavigationBarItem(
+              backgroundColor: Colors.white,
+              icon: _currentIndex == 1
+                  ? new Image.asset('assets/images/medical_sevice_active.png')
+                  : new Image.asset(
+                  'assets/images/medical_sevice_inactive.png'),
+              title: new Text('Health Services',
+                  style: new TextStyle(fontSize: 12.0))),
+          new BottomNavigationBarItem(
+              icon: InkWell(
+                child: Icon(
+                  Icons.format_align_left,
+                  // color: green,
+                  size: 20.0,
+                ),
+              ),
+              title: new Text('History', style: new TextStyle(fontSize: 12.0))),
+        ],
+      ),
+    );
   }
 }
