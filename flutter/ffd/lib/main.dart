7a765effce0a635d255057d8def03c116b66e757
jkbngl <jakob.engl@hotmail.de>
2019-12-21 21:23:27 +0100

    #21 #11 other try with multiple routes

--- flutter/ffd/lib/main.dart
+++ flutter/ffd/lib/main.dart
@@ -1,60 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'route_to_new_page_bottom_nav_task-19.dart';
 
-class AppFooter extends StatefulWidget {
-  @override
-  _AppFooterState createState() => _AppFooterState();
-}
+void main() => runApp(MyApp());
 
-class _AppFooterState extends State<AppFooter> {
-  int _currentIndex = 0;
-
-  List<Widget> _pages = [
-    Text("page1"),
-    Text("page2"),
-    Text("page3"),
-  ];
-
-
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _pages[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        currentIndex: _currentIndex,
-        onTap: (int index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-        items: [
-          new BottomNavigationBarItem(
-              backgroundColor: Colors.white,
-              icon: _currentIndex == 0
-                  ? new Image.asset('assets/images/dashboard_active.png')
-                  : new Image.asset('assets/images/dashboard_inactive.png'),
-              title:
-              new Text('Dashboard', style: new TextStyle(fontSize: 12.0))),
-          new BottomNavigationBarItem(
-              backgroundColor: Colors.white,
-              icon: _currentIndex == 1
-                  ? new Image.asset('assets/images/medical_sevice_active.png')
-                  : new Image.asset(
-                  'assets/images/medical_sevice_inactive.png'),
-              title: new Text('Health Services',
-                  style: new TextStyle(fontSize: 12.0))),
-          new BottomNavigationBarItem(
-              icon: InkWell(
-                child: Icon(
-                  Icons.format_align_left,
-                  // color: green,
-                  size: 20.0,
-                ),
-              ),
-              title: new Text('History', style: new TextStyle(fontSize: 12.0))),
-        ],
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
       ),
+      //home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: NavToNewPageBottomNav(),
     );
   }
 }
