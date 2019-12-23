a7539398a5bfa917e27ce1c406f4bd2caec42fcc
jkbngl <jakob.engl@hotmail.de>
2019-12-23 22:23:13 +0100

    #21 test with alert dialog

--- flutter/ffd/lib/backup_color_change_on_nav_tab.dart
+++ flutter/ffd/lib/backup_color_change_on_nav_tab.dart
@@ -94,6 +94,25 @@ class _MyHomePageState extends State<MyHomePage> {
                   setState(() {
                     dropdownValue = newValue;
                   });
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      // return object of type Dialog
+                      return AlertDialog(
+                        title: new Text("Alert Dialog title"),
+                        content: new Text("Alert Dialog body"),
+                        actions: <Widget>[
+                          // usually buttons at the bottom of the dialog
+                          new FlatButton(
+                            child: new Text("Close"),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                        ],
+                      );
+                    },
+                  );
                 },
                 items: <String>['One', 'Two', 'Free', 'Four']
                     .map<DropdownMenuItem<String>>((String value) {
