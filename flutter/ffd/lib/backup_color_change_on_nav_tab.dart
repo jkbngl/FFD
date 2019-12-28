0b55f121055338586a3de03d3a6e7c980d3aa812
jkbngl <jakob.engl@hotmail.de>
2019-12-28 17:48:20 +0100

    #27 assigned the new scheme to the primarySwatch

--- flutter/ffd/lib/backup_color_change_on_nav_tab.dart
+++ flutter/ffd/lib/backup_color_change_on_nav_tab.dart
@@ -11,13 +11,32 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: colorCustom,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
 
       );
   }
 }
+
+// To assign primarySwatch a custom color
+Map<int, Color> color =
+{
+  50:Color.fromRGBO(136,14,79, .1),
+  100:Color.fromRGBO(136,14,79, .2),
+  200:Color.fromRGBO(136,14,79, .3),
+  300:Color.fromRGBO(136,14,79, .4),
+  400:Color.fromRGBO(136,14,79, .5),
+  500:Color.fromRGBO(136,14,79, .6),
+  600:Color.fromRGBO(136,14,79, .7),
+  700:Color.fromRGBO(136,14,79, .8),
+  800:Color.fromRGBO(136,14,79, .9),
+  900:Color.fromRGBO(136,14,79, 1),
+};
+
+
+MaterialColor colorCustom = MaterialColor(0xFF0957FF, color);
+
 
 class SalesData {
   SalesData(this.year, this.sales);
