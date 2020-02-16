83530655237f8226ec34a7623c0b36d8c42cd91d
jkbngl <jakob.engl@hotmail.de>
2020-02-16 21:43:57 +0100

    #21 working gettings the account if on click of barchart

--- flutter/ffd/lib/backup_color_change_on_nav_tab.dart
+++ flutter/ffd/lib/backup_color_change_on_nav_tab.dart
@@ -298,7 +298,7 @@ class _MyHomePageState extends State<MyHomePage>
 
     for (var amounts in parsedAmounts) {
       visualizerData.add(CompanySizeVsNumberOfCompanies(
-          amounts['level$level_type'].toString(), amounts['sum'], -1));
+          amounts['level$level_type'].toString(), amounts['sum'], amounts['level${level_type.toString()}_fk']));
     }
 
     final desktopTargetLineData = [
@@ -1163,7 +1163,7 @@ class _MyHomePageState extends State<MyHomePage>
       selectedDatum.forEach((charts.SeriesDatum datumPair) {
         print(datumPair.datum.numberOfCompanies);
         print(datumPair.datum.companySize);
-
+        print(datumPair.datum.accountId);
       });
     }
 
