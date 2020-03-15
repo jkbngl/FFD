38feae7b0ec4ec1b3ced6ad23477867358485424
jkbngl <jakob.engl@hotmail.de>
2020-03-15 21:32:43 +0100

    #73 added check that / route endpoint will not be queried more often than x Times

--- Backend/server.py
+++ Backend/server.py
@@ -1,7 +1,12 @@
+from flask import Flask
+
 from flask import render_template
 from flask_cors import CORS
 from connexion.resolver import RestyResolver
 import connexion
+
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 
 # http://192.168.0.21:5000/api/ui/#/
@@ -14,8 +19,16 @@ CORS(app.app)
 # Read the swagger.yml file to configure the endpoints
 app.add_api('swagger.yml')
 
+application = app.app
+
+
+limiter = Limiter(key_func=get_remote_address)
+limiter.init_app(application)
+
+
 # Create a URL route in our application for "/"
 @app.route('/')
+@limiter.limit("100/day;10/hour;1/minute")
 def home():
     """
     This function just responds to the browser ULR
